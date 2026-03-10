196ad3017e99a7a6240d3aed4484a090250dcfc6
kgj <konrad.gjerstad@gmail.com>
2026-03-10 13:41:47 +0100

    add shfmt

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -40,6 +40,8 @@ return {
         typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         css = { 'prettierd', 'prettier', stop_after_first = true },
         -- rust = { 'rustfmt' },
+        bash = { 'shfmt' },
+        sh = { 'shfmt' },
       },
       formatters = {},
     },
